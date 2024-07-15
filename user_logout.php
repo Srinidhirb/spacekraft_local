d44b8ad9791e73a49f77d705244eec778bcb8fd9
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2024-07-15 12:00:07 +0530

    s

--- user_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'connect.php';
-echo '<script>document.cookie = "user_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";</script>';
-echo '<script>window.location.href = "index.php";</script>';
-exit();
-?>
